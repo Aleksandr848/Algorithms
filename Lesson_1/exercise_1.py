d68bd96ea74ab8e9ef59f9db3aa8aa359bea941b
Aleksandr848 <sm-1@inbox.lv>
2022-03-06 15:56:49 +0200

    Update exercise_1.py

--- Lesson_1/exercise_1.py
+++ Lesson_1/exercise_1.py
@@ -10,4 +10,3 @@ digit_2 = (number - digit_1 * 100) // 10
 digit_3 = (number - digit_1 * 100) % 10
 
 print(f'Сумма цифр равна - {digit_1 + digit_2 + digit_3}.\nПроизведение цифр равно - {digit_1 * digit_2 * digit_3}')
-9UpcsaZpfJp1tBoM0lorkABzTPq4lZ
